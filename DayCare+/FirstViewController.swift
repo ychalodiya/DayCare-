10e63ba77b34149ad584064ed1b3d32e40f79ec8
ychalodiya <ychalodiya@gmail.com>
2016-09-14 10:56:50 -0400

    gng

--- DayCare+/FirstViewController.swift
+++ DayCare+/FirstViewController.swift
@@ -14,6 +14,7 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print ("Hi")
+        print("yogi")
     }
 
     override func didReceiveMemoryWarning() {
