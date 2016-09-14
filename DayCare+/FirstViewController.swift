a6d405b2840c9a42ca886bd7ba752bae510c20f8
ychalodiya <ychalodiya@gmail.com>
2016-09-14 11:52:22 -0400

    master Branch

--- DayCare+/FirstViewController.swift
+++ DayCare+/FirstViewController.swift
@@ -13,9 +13,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print ("Hi")
-        print("yogi")
-        print("one more")
+        
     }
 
     override func didReceiveMemoryWarning() {
