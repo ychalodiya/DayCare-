97dc8a09f2cb76fe09c82de5f817ef926fe75a35
Yug Patel <ychalodiya@gmail.com>
2016-09-13 20:34:27 -0400

    commit

---
+++ DayCare+/MainTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBar.swift
+//  DayCare+
+//
+//  Created by Yogesh Chalodiya on 2016-09-12.
+//  Copyright © 2016 Balantech. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBarController {
+    @IBInspectable var defaultIndex: Int = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedIndex = defaultIndex
+    }
+
+
+}
---
+++ DayCare+/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DayCare+
+//
+//  Created by Yogesh Chalodiya on 2016-09-12.
+//  Copyright © 2016 Balantech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
